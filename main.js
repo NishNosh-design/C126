c5b2c019dac7af4c0cf151b72803a44602d9af1a
NishNosh-design <78135240+NishNosh-design@users.noreply.github.com>
2023-08-03 20:59:33 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -12,6 +16,27 @@ function setup() {
 	video = createCapture(VIDEO);
 	video.hide();
 
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		console.log(results);
+		leftWristX = results[0].pose.leftWrist.x;
+		leftWristY = results[0].pose.leftWrist.y;
+		console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY);
+
+		rightWristX = results[0].pose.rightWrist.x;
+		rightWristY = results[0].pose.rightWrist.y;
+		console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY);
+	}
+}
+
+function modelLoaded() {
+	console.log('PoseNet is Initialized');
 }
 
 
@@ -21,5 +46,6 @@ function draw() {
 function play()
 {
 	song.play();
-	
+	song.setVolume(1);
+	song.rate(1);
 }
